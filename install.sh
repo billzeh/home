bb6855d2b632400f3727114b880bbdd3b0098930
Bill Zeh <bill.j.zeh@gmail.com>
2022-01-03 13:37:14 -0500

    Add curl to linux install list

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 
 if [ "$(uname 2> /dev/null)" = "Linux" ]; then
   dconf load /org/gnome/terminal/legacy/profiles:/ < $1/gnome-terminal-profiles.dconf
-  sudo apt install xclip zsh fonts-powerline
+  sudo apt install curl xclip zsh fonts-powerline
   if [ ! -d ~/.oh-my-zsh/ ]; then
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   fi
